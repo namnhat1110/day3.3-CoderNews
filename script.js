55e0578e3c7c210028d6c4cd053a48276f5e4e18
NhatNam <namnhat1110@gmail.com>
2021-04-07 16:31:39 +0700

    attempt for Pre and New Es6

--- script.js
+++ script.js
@@ -1,27 +1,44 @@
 const newurl = "https://newsapi.org/v2/top-headlines?country=us&apiKey=d4c95a5955e4489c9c05b8b3a4f2e43b";
 
-// 1 Old Way
-
 function renderArticles(articles) {
     const newsList = document.getElementById("news-list")
-    console.log(newsList)
+    const articlesHTMLArray = articles.map((a) => {
+        return `
+        <div class="card mb-5" style="width: 30rem;">
+            <img src="${a.urlToImage}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${a.title}</h5>
+                <p class="card-text">${a.description}</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+        `
+    })
+    console.log(articlesHTMLArray)
+    newsList.innerHTML = articlesHTMLArray.join(``)
 }
 
-function fetchData() {
+
+
+// 1) Old Way Pre Es6
+
+function fetchDataPreEs6() {
     const promise = fetch((newurl))
     let articles
     promise
         .then(res => res.json())
         .then(data => renderArticles(data.articles))
-
 }
-
-
-fetchData()
-
+// fetchDataPreEs6()
 
 
 
 
+// 2) New Way Post Es6
 
-// Es6 Way
+async function fetchDataES6AsyncAwait() {
+    const response = await fetch(newurl);
+    const data = await response.json()
+    renderArticles(data.articles)
+}
+fetchDataES6AsyncAwait()
